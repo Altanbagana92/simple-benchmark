1898e5530982da8dcc2833d2814eb247c5be69db
Altanbagana92 <57874164+Altanbagana92@users.noreply.github.com>
2019-11-17 22:05:05 +0100

    Update memsweep.sh

--- memsweep.sh
+++ memsweep.sh
@@ -2,7 +2,7 @@
 EXECUTABLE="memsweep"
 if [ ! -e $EXECUTABLE ] ; then
 	echo "Compiling memsweep.c (requires GNU compiler collection) "
-	gcc -O -o memsweep memsweep.c -lm
+	gcc -O -o memsweep.c -lm
 fi
 
 echo "Running memsweep benchmark"
